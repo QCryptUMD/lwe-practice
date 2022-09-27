7d9f541d245c6c1d260699cf4cb1e19ee6303f84
Adnan-Benchaaboun <adnanben@comcast.net>
2022-09-26 20:14:42 -0400

    First Commit

--- cryptography/keygen.py
+++ cryptography/keygen.py
@@ -1,1 +1,18 @@
 # TODO
+
+import random
+
+arr=[]
+rows, cols=3,3
+for i in range(rows):
+    col = []
+    for j in range(cols):
+
+        polynomials = [random.randint(0,255)]
+
+        for x in range(0, len(polynomials) - 1):
+            polynomials[x] = random.randint(0,1)
+        col.append(polynomials)
+    arr.append(col)
+print(arr)
+
