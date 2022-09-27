3bdeb37c317d04b69863ef78eed26a625f9038c3
sahilg24 <99225264+sahilg24@users.noreply.github.com>
2022-09-27 17:15:01 -0400

    Made it into a class

--- maths/error_generation.py
+++ maths/error_generation.py
@@ -1,13 +1,22 @@
+import polynomial
 import random
-coefficients = []
-for i in range(768):
-  sum = 0
-  for i in range(6):
-    if i > 2:
-      sum -= random.randrange(0,2)
-    else:
-      sum += random.randrange(0,2)
-  coefficients.append(sum)
-error_components = [Polynomial.from_coefficients(coefficients[:256]), 
-                    Polynomial.from_coefficients(coefficients[256:512]), 
-                    Polynomial.from_coefficients(coefficients[512:])]
+
+class ErrorGenerator():
+    @staticmethod
+    def generate():
+      coefficients = []
+
+      for i in range(768):
+        sum = 0
+        for i in range(6):
+          if i > 2:
+            sum -= random.randrange(0,2)
+          else:
+            sum += random.randrange(0,2)
+        coefficients.append(sum)
+
+      return [Polynomial.from_coefficients(coefficients[:256]), 
+              Polynomial.from_coefficients(coefficients[256:512]), 
+              Polynomial.from_coefficients(coefficients[512:])]
+
+    
