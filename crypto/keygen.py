ae272406e0be635a80bdd9f5d5a2fd1c0325ee37
Alex Yelovich <alex@yeldev>
2022-09-28 16:24:36 -0400

    Implemented keygen, though have not tested it yet

--- crypto/keygen.py
+++ crypto/keygen.py
@@ -1,42 +1,46 @@
-# TODO
-
 from maths.polynomial import Polynomial
 from maths.linalg import Matrix
+from maths.error_generation import ErrorGenerator
 import random
 
-"""
-arr=[]
-rows, cols=3,3
-for i in range(rows):
-    col = []
-    for j in range(cols):
+#Returns a tuple with (public_key, secret_key)
+def keygen():
+    #Generate the matrix first
 
-        rand_val = random.randint(0,255)
-        polynomials = []
+    RAND_MAT_SIZE = 3
+    MAX_DEGREE = 255
+    COEFF_MOD = 1
 
-        for x in range(0, rand_val):
-            polynomials.append(random.randint(0,1))
-        col.append(Polynomial.from_coefficients(polynomials))
-    arr.append(col)
-print(arr)
-"""
+    matA = [ [] for _ in range(RAND_MAT_SIZE) ]
+    
+    for row in range(len(matA)):
+        for col in range(RAND_MAT_SIZE):
+            degree = random.randint(0, MAX_DEGREE)
+            poly = [ random.randint(0, 1) for _ in range(degree + 1) ]
 
-#Generate the matrix first
+            matA[row].append(Polynomial.from_coefficients(poly))
+            
+    matA = Matrix(matA)
 
-RAND_MAT_SIZE = 3
-MAX_DEGREE = 255
-COEFF_MOD = 1
+    # Now generate the secret and error term
 
-matA = [ [] for _ in range(RAND_MAT_SIZE) ]
+    secret = [ [] for _ in range(RAND_MAX_SIZE) ]
 
-for row in range(len(matA)):
-    for col in range(RAND_MAT_SIZE):
+    for row in range(len(secret)):
         degree = random.randint(0, MAX_DEGREE)
-        poly = [ random.randint(0, 1) for _ in range(degree + 1) ]
+        poly = [ random.randint(0, 1) for _ in range (degree + 1) ]
 
-        matA[row].append(Polynomial.from_coefficients(poly))
+        secret.append(Polynomial.from_coefficients(poly))
 
-matA = Matrix(matA)
+    secret = Matrix(secret)
+
+    errGen = ErrorGenerator()
+
+    error = errGen.generate()
+
+    public_key = (matA.multiply(secret).add(error), matA)
+
+    return (public_key, secret)
 
 #print(matA.matrix)
 
